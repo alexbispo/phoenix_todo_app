0e4802b2f207db6d8d2c4bd6b61694065e2fcc66
alexbispo <alexbispo.artes@gmail.com>
2019-12-09 22:51:30 -0200

    Use routes helpers.

--- lib/phoenix_todo_app_web/views/tasks_view.ex
+++ lib/phoenix_todo_app_web/views/tasks_view.ex
@@ -1,6 +1,8 @@
 defmodule PhoenixTodoAppWeb.TasksView do
   use PhoenixTodoAppWeb, :view
 
+  alias PhoenixTodoAppWeb.Router.Helpers, as: Routes
+
   def format_status(0), do: "Pendente"
   def format_status(1), do: "Feito"
 end
